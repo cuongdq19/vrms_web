fd5afbc9e1dd04e72deff65057ca9871981292f5
Cuong Dang <cuong.developer19@gmail.com>
2020-12-23 14:32:59 +0700

    Changed UI Register Page.

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,20 +1,12 @@
 import { faFileUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  Button,
-  Form,
-  Input,
-  InputNumber,
-  Select,
-  TimePicker,
-  Typography,
-  Upload,
-} from 'antd';
-import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Form, Input, message, Typography, Upload } from 'antd';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import http from '../http';
 import CustomMap from '../components/CustomMap';
+import { Redirect } from 'react-router-dom';
 
 const Container = styled.div`
   width: 100vw;
@@ -37,23 +29,14 @@ const FormButtons = styled.div`
   align-items: center;
 `;
 
-const { Option } = Select;
-
 const Register = () => {
-  const [manufacturers, setManufacturers] = useState([]);
   const [form] = Form.useForm();
+  const [redirect, setRedirect] = useState(null);
 
   const submitHandler = (values) => {
     const formData = new FormData();
     Object.keys(values).forEach((key) => {
       switch (key) {
-        case 'workingTime':
-          formData.append('openTime', values[key][0].unix());
-          formData.append('closeTime', values[key][1].unix());
-          break;
-        case 'slotDuration':
-          formData.append(key, values[key].unix());
-          break;
         case 'images':
           values[key].forEach((img) =>
             formData.append('images', img.originFileObj)
@@ -67,25 +50,17 @@ const Register = () => {
     http
       .post('/contracts', formData)
       .then(({ data }) => {
-        console.log(data);
+        message.success('Request success. We will contact you ASAP.');
+        setRedirect(<Redirect to="/sign-in" />);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  const fetchSelections = useCallback(() => {
-    http.get('/manufacturers').then(({ data }) => {
-      setManufacturers(data);
-    });
-  }, []);
-
-  useEffect(() => {
-    fetchSelections();
-  }, [fetchSelections]);
-
   return (
     <Container>
+      {redirect}
       <FormContainer>
         <div style={{ textAlign: 'center' }}>
           <Typography.Title level={4}>
@@ -93,7 +68,6 @@ const Register = () => {
           </Typography.Title>
         </div>
         <Form form={form} layout="vertical" onFinish={submitHandler}>
-          <Typography.Title level={4}>Contact</Typography.Title>
           <Form.Item label="Full Name" name="fullName">
             <Input />
           </Form.Item>
@@ -102,39 +76,6 @@ const Register = () => {
           </Form.Item>
           <Form.Item label="Phone Number" name="phoneNumber">
             <Input />
-          </Form.Item>
-          <Typography.Title level={4}>Provider</Typography.Title>
-          <Form.Item label="Name" name="providerName">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Manufacturer" name="manufacturerId">
-            <Select>
-              {manufacturers.map((m) => (
-                <Option key={m.id} value={m.id}>
-                  {m.name}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item label="Working Time" name="workingTime">
-            <TimePicker.RangePicker
-              style={{ width: '100%' }}
-              hourStep={1}
-              minuteStep={15}
-              format="HH:mm"
-            />
-          </Form.Item>
-          <Form.Item label="Slot Duration (minutes)" name="slotDuration">
-            <TimePicker
-              style={{ width: '100%' }}
-              showNow={false}
-              hourStep={1}
-              minuteStep={5}
-              format="HH:mm"
-            />
-          </Form.Item>
-          <Form.Item label="Slot Capacity" name="slotCapacity">
-            <InputNumber type="number" style={{ width: '100%' }} />
           </Form.Item>
           <Form.Item
             name="images"
@@ -161,16 +102,10 @@ const Register = () => {
               </Button>
             </Upload>
           </Form.Item>
-          <Form.Item name="latitude" hidden>
-            <Input />
-          </Form.Item>
-          <Form.Item name="longitude" hidden>
-            <Input />
-          </Form.Item>
           <Form.Item name="address" label="Address">
             <CustomMap
               onSearch={(address, latitude, longitude) =>
-                form.setFieldsValue({ address, latitude, longitude })
+                form.setFieldsValue({ address })
               }
             />
           </Form.Item>
