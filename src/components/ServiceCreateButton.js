fb614324303ff99e06f0cc9e9809b51657b90f83
Cuong Dang <cuong.developer19@gmail.com>
2020-12-26 00:27:52 +0700

    Finalize Create Service UI

--- src/components/ServiceCreateButton.js
+++ src/components/ServiceCreateButton.js
@@ -1,5 +1,16 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Button, Col, Form, Input, message, Modal, Row, Select } from 'antd';
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  InputNumber,
+  message,
+  Modal,
+  Row,
+  Select,
+  Typography,
+} from 'antd';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -14,10 +25,11 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
   const [serviceTypes, setServiceTypes] = useState([]);
   const [serviceTypeDetails, setServiceTypeDetails] = useState([]);
   const [categories, setCategories] = useState([]);
-  // const [manufacturers, setManufacturers] = useState([]);
-  // const [models, setModels] = useState([]);
+  const [manufacturers, setManufacturers] = useState([]);
+  const [models, setModels] = useState([]);
   const [parts, setParts] = useState([]);
   const [selectedParts, setSelectedParts] = useState([]);
+  const [disabledModels, setDisabledModels] = useState(false);
 
   const clickedHandler = () => {
     setVisible(true);
@@ -26,20 +38,23 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
   const closedHandler = () => {
     form.resetFields();
     setSelectedParts([]);
+    setParts([]);
     setVisible(false);
   };
 
   const serviceTypeChangedHandler = (typeId) => {
+    setDisabledModels(typeId === 3);
+
     http
       .post('/service-type-details', [typeId])
       .then(({ data }) => setServiceTypeDetails(data));
   };
 
-  // const manufacturerChangedHandler = (manuId) => {
-  //   http
-  //     .get(`/models/manufacturers/${manuId}`)
-  //     .then(({ data }) => setModels(data));
-  // };
+  const manufacturerChangedHandler = (manuId) => {
+    http
+      .get(`/models/manufacturers/${manuId}`)
+      .then(({ data }) => setModels(data));
+  };
 
   const sectionChangedHandler = (sectionId) => {
     http
@@ -73,6 +88,17 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
       updatedSelected.push({ ...part, quantity: 1 });
     }
     setSelectedParts(updatedSelected);
+
+    const selectedModels = form.getFieldValue('modelIds');
+    if (!selectedModels) {
+      const { models, categoryId } = part;
+      form.setFieldsValue({ modelIds: models.map((m) => m.id) });
+      http
+        .get(`/parts/provider/${providerId}/vehicle-model/${models[0].id}`)
+        .then(({ data }) => {
+          setParts(data.filter((part) => part.categoryId === categoryId));
+        });
+    }
   };
 
   const partRemovedHandler = (partId) => {
@@ -95,17 +121,31 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
   };
 
   const submitHandler = (values) => {
-    const { typeDetailId, modelIds, name, price } = values;
-    const reqBody = {
+    const { typeId, typeDetailId, modelIds, name, price } = values;
+    let url = `/services/providers/${providerId}/replacing`;
+    let reqBody = {
       typeDetailId,
-      groupPriceRequest: {
-        modelIds,
+    };
+    if (typeId === 3) {
+      // Replace
+      const partQuantity = {};
+      selectedParts.forEach((part) => {
+        partQuantity[part.id] = part.quantity;
+      });
+      reqBody.groupPriceRequest = {
+        partQuantity,
         name,
         price,
-      },
-    };
+      };
+    } else {
+      url = `/services/providers/${providerId}/non-replacing`;
+      reqBody.serviceName = name;
+      reqBody.price = price;
+      reqBody.modelIds = modelIds;
+    }
+
     http
-      .post(`/services/providers/${providerId}`, reqBody)
+      .post(url, reqBody)
       .then(({ data }) => {
         message.success('Create service success.');
         closedHandler();
@@ -116,14 +156,17 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
 
   const fetchSelections = useCallback(() => {
     if (visible) {
-      http.get('/service-types').then(({ data }) => {
-        setServiceTypes(data);
-      });
-      // .then(({ data }) => {
-      //   setManufacturers(data);
-      //   return http.get('/models');
-      // })
-      // .then(({ data }) => setModels(data));
+      http
+        .get('/service-types')
+        .then(({ data }) => {
+          setServiceTypes(data);
+          return http.get('/manufacturers');
+        })
+        .then(({ data }) => {
+          setManufacturers(data);
+          return http.get('/models');
+        })
+        .then(({ data }) => setModels(data));
     }
   }, [visible]);
 
@@ -147,8 +190,8 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
       >
         <Form form={form} layout="vertical" onFinish={submitHandler}>
           <Row gutter={8}>
-            <Col span={10}>
-              <Form.Item label="Service Type">
+            <Col span={12}>
+              <Form.Item name="typeId" label="Service Type">
                 <Select onChange={serviceTypeChangedHandler}>
                   {serviceTypes.map((st) => (
                     <Option key={st.id} value={st.id}>
@@ -158,7 +201,7 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
                 </Select>
               </Form.Item>
             </Col>
-            <Col span={14}>
+            <Col span={12}>
               <Form.Item name="typeDetailId" label="Section Name">
                 <Select
                   disabled={serviceTypeDetails.length === 0}
@@ -178,10 +221,13 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
           <Form.Item name="name" label="Service Name">
             <Input placeholder="Enter service name" />
           </Form.Item>
-          {/* <Row gutter={8}>
+          <Row gutter={8}>
             <Col span={4}>
               <Form.Item label="Manufacturer">
-                <Select onChange={manufacturerChangedHandler}>
+                <Select
+                  disabled={disabledModels}
+                  onChange={manufacturerChangedHandler}
+                >
                   {manufacturers.map((m) => (
                     <Option key={m.id} value={m.id}>
                       {m.name}
@@ -192,7 +238,7 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
             </Col>
             <Col span={16}>
               <Form.Item label="Models" name="modelIds">
-                <Select mode="multiple">
+                <Select disabled={disabledModels} mode="multiple">
                   {models.map((model) => (
                     <Option key={model.id} value={model.id}>
                       {model.manufacturerName} {model.name} {model.fuelType}{' '}
@@ -207,9 +253,9 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
                 <InputNumber style={{ width: '100%' }} type="number" min={0} />
               </Form.Item>
             </Col>
-          </Row> */}
+          </Row>
           <Row gutter={8}>
-            <Col span={10}>
+            <Col span={12}>
               <Row>
                 <Col span={24}>
                   <Form.Item name="categoryId" label="Category">
@@ -225,32 +271,45 @@ const ServiceCreateButton = ({ children, onSuccess }) => {
                     </Select>
                   </Form.Item>
                 </Col>
-                {selectedParts.map((part) => (
+                <Col span={24}>
+                  {selectedParts.map((part) => (
+                    <Row key={part.id} gutter={[8, 8]}>
+                      <Col span={12}>
+                        <Typography.Text>{part.name}</Typography.Text>
+                      </Col>
+                      <Col span={4}>
+                        <Typography.Text>{part.price}</Typography.Text>
+                      </Col>
+                      <Col span={4}>
+                        <Typography.Text>{part.quantity}</Typography.Text>
+                      </Col>
+                      <Col span={4}>
+                        <Button
+                          danger
+                          onClick={() => partRemovedHandler(part.id)}
+                        >
+                          Remove
+                        </Button>
+                      </Col>
+                    </Row>
+                  ))}
+                </Col>
+              </Row>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="Parts">
+                {parts.map((part) => (
                   <Row key={part.id} gutter={[8, 8]}>
-                    <Col span={6}>{part.name}</Col>
-                    <Col span={6}>{part.price}</Col>
-                    <Col span={6}>{part.quantity}</Col>
-                    <Col span={6}>
-                      <Button onClick={() => partRemovedHandler(part.id)}>
-                        Remove
+                    <Col span={16}>{part.name}</Col>
+                    <Col span={4}>{part.price}</Col>
+                    <Col span={4}>
+                      <Button onClick={() => partSelectedHandler(part)}>
+                        Select
                       </Button>
                     </Col>
                   </Row>
                 ))}
-              </Row>
-            </Col>
-            <Col span={14}>
-              {parts.map((part) => (
-                <Row key={part.id} gutter={[8, 8]}>
-                  <Col span={8}>{part.name}</Col>
-                  <Col span={8}>{part.price}</Col>
-                  <Col span={8}>
-                    <Button onClick={() => partSelectedHandler(part)}>
-                      Select
-                    </Button>
-                  </Col>
-                </Row>
-              ))}
+              </Form.Item>
             </Col>
           </Row>
         </Form>
