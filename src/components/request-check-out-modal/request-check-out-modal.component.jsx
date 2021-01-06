1eb955ac3dde8b212d529023bd478300c46c823b
Cuong Dang <cuong.developer19@gmail.com>
2021-01-06 17:26:46 +0700

    Refactor UI Checkout Modal.

--- src/components/request-check-out-modal/request-check-out-modal.component.jsx
+++ src/components/request-check-out-modal/request-check-out-modal.component.jsx
@@ -1,9 +1,11 @@
-import { message, Modal } from 'antd';
+import { Col, message, Modal, Row } from 'antd';
 import React from 'react';
 import { connect } from 'react-redux';
 
-import RequestOverview from '../request-overview/request-overview.component';
 import * as actions from '../../store/actions';
+import { calculateRequestPrice, formatMoney } from '../../utils';
+import ServicesCollectionTable from '../services-collection-table/services-collection-table.component';
+import { Summary } from './request-check-out-modal.styles';
 
 const RequestCheckoutModal = ({
   visible,
@@ -13,6 +15,11 @@ const RequestCheckoutModal = ({
   onCancel,
 }) => {
   const { id } = item ?? {};
+
+  let total = 0;
+  if (item) {
+    total = calculateRequestPrice(item);
+  }
 
   return (
     <Modal
@@ -31,7 +38,41 @@ const RequestCheckoutModal = ({
         })
       }
     >
-      <RequestOverview item={item} />
+      <Row>
+        <Col span={24}>
+          <ServicesCollectionTable
+            dataSource={item?.services.map(
+              ({ id, serviceName, servicePrice, parts, ...rest }) => ({
+                id,
+                name: serviceName,
+                price: servicePrice,
+                parts: parts.map(({ partId, partName, ...rest }) => ({
+                  id: partId,
+                  name: partName,
+                  ...rest,
+                })),
+              })
+            )}
+            rowKey="id"
+          />
+        </Col>
+        <Col span={8} offset={16}>
+          <Summary justify="end" align="middle" gutter={[8, 8]}>
+            <Col span={6}>
+              <span>Services: </span>
+            </Col>
+            <Col span={18}>
+              <h3>{formatMoney(total.services)}</h3>
+            </Col>
+            <Col span={6}>
+              <span>Total: </span>
+            </Col>
+            <Col span={18}>
+              <h3>{formatMoney(total.total)}</h3>
+            </Col>
+          </Summary>
+        </Col>
+      </Row>
     </Modal>
   );
 };
