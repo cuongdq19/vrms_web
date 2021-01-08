050366bc3ea9a8e95a3ba92307180abd56fcf9df
Cuong Dang <cuong.developer19@gmail.com>
2021-01-08 22:55:43 +0700

    Not logged redux if collapsed keys.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
 import rootSaga from './root-saga';
+import CommonActionTypes from './common/common.types';
 
 const sagaMiddleware = createSagaMiddleware();
+
+const logger = createLogger({
+  predicate: (getState, action) =>
+    action.type !== CommonActionTypes.SIDER_SET_OPEN_KEYS,
+});
 
 const middlewares = [sagaMiddleware, logger];
 
